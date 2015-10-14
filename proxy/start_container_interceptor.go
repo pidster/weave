1b50ecd1b958f46ac4be166c18516ba582c9d220
Bryan Boreham <bjboreham@gmail.com>
2015-10-14 11:13:41 +0100

    Cope with client sending a HostConfig named HostConfig

--- proxy/start_container_interceptor.go
+++ proxy/start_container_interceptor.go
@@ -21,9 +21,16 @@ func (i *startContainerInterceptor) InterceptRequest(r *http.Request) error {
 	// If the client has sent some JSON which might be a HostConfig, add our
 	// parameters back into it, otherwise Docker will consider them overwritten
 	if containerShouldAttach(container) && r.Header.Get("Content-Type") == "application/json" {
-		hostConfig := map[string]interface{}{}
-		if err := unmarshalRequestBody(r, &hostConfig); err != nil {
+		params := map[string]interface{}{}
+		if err := unmarshalRequestBody(r, &params); err != nil {
 			return err
+		}
+		// HostConfig can be sent either unnamed at top level or as a struct named HostConfig
+		hostConfig := params
+		if subParam, found := params["HostConfig"]; found {
+			if typecast, ok := subParam.(map[string]interface{}); ok {
+				hostConfig = typecast
+			}
 		}
 		if len(hostConfig) > 0 {
 			i.proxy.addWeaveWaitVolume(hostConfig)
@@ -33,7 +40,8 @@ func (i *startContainerInterceptor) InterceptRequest(r *http.Request) error {
 				}
 			}
 
-			if err := marshalRequestBody(r, hostConfig); err != nil {
+			// Note we marshal the original top-level dictionary to avoid disturbing anything else
+			if err := marshalRequestBody(r, params); err != nil {
 				return err
 			}
 		}
